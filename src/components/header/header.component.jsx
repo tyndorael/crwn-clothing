7ff7f686f24789fe97ea647332b64081fbdf528a
Alejandro Oyarzún Sepúlveda <aosepulveda@outlook.com>
2019-10-16 19:03:27 -0300

    add cart-dropdown component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,6 +4,7 @@ import { connect } from "react-redux";
 
 import { auth } from "../../firebase/firebase.utils";
 import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import { ReactComponent as Logo} from '../../assets/crown.svg';
 
@@ -29,6 +30,7 @@ const Header = ({ currentUser }) => (
       }
       <CartIcon />
     </div>
+    <CartDropdown />
   </div>
 );
 
